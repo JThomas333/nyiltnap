65c41578075fe4dec79bd71929418cc69caa7482
Tamás Józsa <c0joztam@vasvari.org>
2026-02-04 12:16:55 +0100

    url

--- app.js
+++ app.js
@@ -3,9 +3,10 @@ import db from "./data/db.js";
 
 const app = express();
 const PORT = 3321;
+const URL = "/nyiltnap/api/v1";
 app.use(express.json());
 
-app.get("/telepules", (req, res) => {
+app.get(URL +"/telepules", (req, res) => {
     const nev = req.query.nev;
     const telepules = db.prepare("SELECT * FROM telepulesek WHERE nev = ?").get(nev);
     if (!telepules){
@@ -14,7 +15,7 @@ app.get("/telepules", (req, res) => {
     res.status(200).json(telepules);
 });
 
-app.get("/tanora", (req, res) => {
+app.get(URL + "/tanora", (req, res) => {
     const targy = req.query.targy;
     if (!targy){
         return res.status(400).json({ error: "Hiányzó 'targy' lekérdezési paraméter" });
@@ -23,21 +24,22 @@ app.get("/tanora", (req, res) => {
     res.status(200).json(orak);
 });
 
-app.get("/9-matematika-fizika", (req, res) => {
+app.get(URL + "/9-matematika-fizika", (req, res) => {
     const csoport = req.query.csoport;
     if (!csoport){
         return res.status(400).json({ error: "Hiányzó 'csoport' lekérdezési paraméter" });
     }
-    const matematikaFizikaOrak = db.prepare("SELECT csoport, targy, datum, FROM tanorak WHERE csoport = ? AND (targy = 'matematika' OR targy = 'fizika') ORDER BY targy").all(csoport);
+    const matematikaFizikaOrak = db.prepare("SELECT csoport, targy, datum FROM tanorak WHERE csoport = ? AND (targy = 'matematika' OR targy = 'fizika') ORDER BY targy").all(csoport);
     res.status(200).json(matematikaFizikaOrak);
 });
+
 // településenként hány diák regisztrált?
-app.get("/telepulesfo", (req, res) => {
+app.get(URL + "/telepulesfo", (req, res) => {
     const telepules = db.prepare("SELECT telepules, COUNT(*) AS diak_szam FROM diakok GROUP BY telepules ORDER BY diak_szam DESC").all();
     res.status(200).json(telepules);
 });
 
-app.get("/tantargyak", (req, res) => {
+app.get(URL + "/tantargyak", (req, res) => {
     const visitable = db.prepare(`SELECT targy FROM orak Where ferohely = ?`).all();
     res.status(200).json(visitable);
 });
